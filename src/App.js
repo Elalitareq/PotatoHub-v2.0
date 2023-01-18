ee3f2d9b365ea0bd13d34636513ec7b6868627fe
Tareq ElAli <elalitareq@gmail.com>
2023-01-18 23:27:07 +0200

    finalized website with no animation yet and no components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import phone from "./phone.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <img src={logo} className="App-logo" alt="logo" width={40} />
+        <nav className="App-nav">
+          <a className="navi" href="#">
+            Home
+          </a>
+          <a className="navi" href="#">
+            About
+          </a>
+          <a className="navi" href="#">
+            Blog
+          </a>
+          <a className="navi" href="#">
+            Temps
+          </a>
+        </nav>
+        <a className="navi navi-out" href="#">
+          Contact Us
         </a>
       </header>
+      <main className="App-main">
+        <section className="App-intro">
+          <div>
+            <h1 className="App-title">OUR GOAL</h1><div  className="typewriter"><h2>
+            To give you top <span className="gg">quality</span> potato stories, and increase the number of people that loves <span className="gg">potatoes</span></h2></div>
+          </div>
+          <img src={phone} className="phone" height={700}></img>
+        </section>
+      </main>
+      <footer className="App-footer"></footer>
     </div>
   );
 }
